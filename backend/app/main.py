89e36c358e78b65fbca08806c59713f19bf48a23
Anushka <anushkakaushal2611@gmail.com>
2026-06-17 11:56:33 +0530

    Minimal FastAPI test

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,75 +1,11 @@
-
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-
-
-from app.api.upload import (
-    router as upload_router
-)
-
-from app.api.query import (
-    router as query_router
-)
-
-from app.api.analyze import (
-    router as analyze_router
-)
-
-from app.api.workflow import (
-    router as workflow_router
-)
-
-from app.api.risk import (
-    router as risk_router
-)
-
-
 
 app = FastAPI()
-
-from app.services.scheduler_service import (
-    scheduler
-)
-scheduler.start()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-app.include_router(
-    risk_router,
-    prefix="/api"
-)
-
-app.include_router(
-    upload_router,
-    prefix="/api"
-)
-
-app.include_router(
-    query_router,
-    prefix="/api"
-)
-
-app.include_router(
-    analyze_router,
-    prefix="/api"
-)
-
-app.include_router(
-    workflow_router,
-    prefix="/api"
-)
-
 
 @app.get("/")
 def root():
     return {
-        "message":
-        "CognitiveOps Backend Running"
+        "message": "Backend Running"
     }
 
 @app.get("/debug")
@@ -77,4 +13,3 @@ def debug():
     return {
         "message": "new code running"
     }
-
