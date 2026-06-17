13c2967682a88f8c0b487a0e6048b434c01b6b36
Anushka <anushkakaushal2611@gmail.com>
2026-06-17 12:09:31 +0530

    Minimal + workflow router

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,3 +13,13 @@ def debug():
     return {
         "message": "new code running"
     }
+
+
+from app.api.workflow import (
+    router as workflow_router
+)
+
+app.include_router(
+    workflow_router,
+    prefix="/api"
+)
