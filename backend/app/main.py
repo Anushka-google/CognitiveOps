c4a91f3673f04c0c0b9f71d85240768624f7f95e
Anushka <anushkakaushal2611@gmail.com>
2026-06-17 14:02:52 +0530

    adding only workflow

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,18 +1,18 @@
-from fastapi import FastAPI
+# from fastapi import FastAPI
 
-app = FastAPI()
+# app = FastAPI()
 
-@app.get("/")
-def root():
-    return {
-        "message": "Backend Running"
-    }
+# @app.get("/")
+# def root():
+#     return {
+#         "message": "Backend Running"
+#     }
 
-@app.get("/debug")
-def debug():
-    return {
-        "message": "new code running"
-    }
+# @app.get("/debug")
+# def debug():
+#     return {
+#         "message": "new code running"
+#     }
 
 
 from app.api.workflow import (
