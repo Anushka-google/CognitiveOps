8297019cd3a417fc249ab2c845527ce3c566be08
Anushka <anushkakaushal2611@gmail.com>
2026-06-18 09:47:46 +0530

    minimal main test

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,25 +1,7 @@
-# from fastapi import FastAPI
+from fastapi import FastAPI
 
-# app = FastAPI()
+app = FastAPI()
 
-# @app.get("/")
-# def root():
-#     return {
-#         "message": "Backend Running"
-#     }
-
-# @app.get("/debug")
-# def debug():
-#     return {
-#         "message": "new code running"
-#     }
-
-
-from app.api.workflow import (
-    router as workflow_router
-)
-
-app.include_router(
-    workflow_router,
-    prefix="/api"
-)
+@app.get("/")
+def root():
+    return {"message": "working"}
